a63cb4f143dc3c318c23b5da25d5aa182d7d9034
Anirudh Deshpande <adeshpande@salud.unm.edu>
2016-12-02 20:14:31 -0700

    Update user list component to display list of users from the mock data

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { UserListComponent } from './user-list.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,HttpModule ],
   declarations: [ AppComponent, UserListComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [ HttpModule ]
 })
 export class AppModule { }
--- client/app/user-list.component.ts
+++ client/app/user-list.component.ts
@@ -1,8 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import { User } from './user';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'user-list',
   templateUrl: 'app/user-list.component.html',
 })
-export class UserListComponent {
+
+export class UserListComponent implements OnInit {
+	users: User[];
+
+	constructor(private http: Http) { }
+
+	ngOnInit(): void {
+		this.getUsers();
+	}
+
+	getUsers() {
+		this.http.get('app/mock-data/data.json').map(res => res.json()).subscribe(
+			data => {this.users = data.users;}
+		);	
+	}
+	
 }
