b30b87d14620dcd02c79dae267942e78904041cd
Anirudh Deshpande <adeshpande@salud.unm.edu>
2016-12-02 19:31:48 -0700

    Add user list component and include the directive in the app component

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { UserListComponent } from './user-list.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, UserListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ client/app/user-list.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'user-list',
+  templateUrl: 'app/user-list.component.html',
+})
+export class UserListComponent {
+}
