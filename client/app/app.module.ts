6112fdd311590f5fc98641bd6d63410eb65029d0
Anirudh Deshpande <adeshpande@salud.unm.edu>
2016-12-03 13:52:26 -0700

    Comment out user detail component import code

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -6,11 +6,11 @@ import { HttpModule } from '@angular/http';
 import {UserSearchPipe} from "./user-search-pipe";
 import {Ng2PaginationModule} from 'ng2-pagination';
 import { RouterModule, Routes } from '@angular/router';
-import { UserDetailComponent } from './user-detail.component';
+//import { UserDetailComponent } from './user-detail.component';
 
-const appRoutes: Routes = [
+/*const appRoutes: Routes = [
   { path: 'user/:id', component: UserDetailComponent }
-];
+];*/
 
 
 @NgModule({
