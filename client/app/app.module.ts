c379a380279e9255bbb30815d0e60d06c143449d
Anirudh Deshpande <adeshpande@salud.unm.edu>
2016-12-03 13:49:32 -0700

    Add pagination to users list

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent }  from './app.component';
 import { UserListComponent } from './user-list.component';
 import { HttpModule } from '@angular/http';
 import {UserSearchPipe} from "./user-search-pipe";
+import {Ng2PaginationModule} from 'ng2-pagination';
+import { RouterModule, Routes } from '@angular/router';
+import { UserDetailComponent } from './user-detail.component';
+
+const appRoutes: Routes = [
+  { path: 'user/:id', component: UserDetailComponent }
+];
+
 
 @NgModule({
-  imports:      [ BrowserModule,HttpModule ],
+  imports:      [ BrowserModule,HttpModule, Ng2PaginationModule ],
   declarations: [ AppComponent, UserListComponent,UserSearchPipe ],
   bootstrap:    [ AppComponent ],
   providers: [ HttpModule ]
